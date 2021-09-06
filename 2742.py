e7641175cf478d9948bf09418c15a6eaa32cf5e0
권구현 <61926067+nine-hyeon@users.noreply.github.com>
2021-09-06 12:00:36 +0900

    Create 2742.py

---
+++ 2742.py
@@ -0,0 +1,15 @@
+n = input("100,000 이하의 자연수를 입력해주세요.")
+
+if not n.isdigit():
+
+    print("자연수를 입력해주세요.")
+
+elif int(n) > 100000:
+
+    print("n은 100,000이하여야 합니다.")
+
+else:
+
+    for i in range(0, int(n)):
+
+        print(int(n) - i)
